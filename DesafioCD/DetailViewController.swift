439bd78eee1109b75eabd845d9d5eb3c219a62d1
ios7126 <ios7126@caelums-mac-mini-2.local>
2017-09-19 21:54:31 -0300

    Initial Commit

---
+++ DesafioCD/DetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailViewController.swift
+//  DesafioCD
+//
+//  Created by ios7126 on 9/19/17.
+//  Copyright © 2017 Caelum. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = self.detailItem {
+            if let label = self.detailDescriptionLabel {
+                label.text = detail.timestamp!.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    var detailItem: Event? {
+        didSet {
+            // Update the view.
+            self.configureView()
+        }
+    }
+
+
+}
+
